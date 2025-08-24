1f137ae686879da517878018d9b10aa019eeb262
ranadebsaha <ranadebsaha2002@gmail.com>
2025-08-24 13:58:45 +0530

    by rds

--- backend/demand/urls.py
+++ backend/demand/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import DemandCreateView,UserProfileView,UserLoginView,UserRegisterView,test_django_email_settings
+from .views import DemandCreateView,UserProfileView,UserLoginView,UserRegisterView
 
 urlpatterns = [
     path("demand/create/", DemandCreateView.as_view(), name="create-demand"),
